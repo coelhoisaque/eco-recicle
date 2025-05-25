af103972789c3befcdcfcc6d7f5d7152bf11b30a
Isaque <isaquecoelho.dj@gmail.com>
2025-05-25 17:39:39 -0300

    refactor: ajusta componente Footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,14 +1,16 @@
-import "./style.css"
+import "./style.css";
 const Footer = () => {
-    return(
-        <div className="BoxFooter">
-            <h5 className="devTitulo">Desenvolvedores</h5>
-            <a href="https://www.instagram.com/heric_viana_s/" target="_blanck" className="linkFooter">@heric_viana_s</a>
-            <a href="https://www.instagram.com/aricia_ferreira/" target="_blanck" className="linkFooter">@aricia_ferreira</a>
-            <a href="https://www.instagram.com/isaque_cdj/" target="_blanck" className="linkFooter">@isaque_cdj</a>
-            <a href="https://www.instagram.com/nallanda_novato/" target="_blanck" className="linkFooter">@nallanda_novato</a>
-            <a href="https://www.instagram.com/robert_cardosoo//" target="_blanck" className="linkFooter">@robert_cardosoo</a>
-        </div>
-    )
-}
+  return (
+    <div className="BoxFooter">
+      <h5 className="devTitulo">Desenvolvedor</h5>
+      <a
+        href="https://www.instagram.com/isaque_cdj/"
+        target="_blanck"
+        className="linkFooter"
+      >
+        @isaque_cdj
+      </a>
+    </div>
+  );
+};
 export default Footer;
